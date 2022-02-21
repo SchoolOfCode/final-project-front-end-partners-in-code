e3fca49137e22a402680243649969b1970bd8a02
Alex Mulgrew <alexpmulgrew@gmail.com>
2022-02-21 16:41:32 +0000

    Pop up Modal started

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+
+		
+        
+        
+        
+        
+        
+        
+        
+        
+        renders:
+		p tag <p>{phone}</p> (for phone number)
+		p tag <p>{email}</p> (for email)
+		props:
+		phone
+		email
