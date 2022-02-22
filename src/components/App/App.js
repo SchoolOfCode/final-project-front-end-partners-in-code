b41e0a8cf2f9aebf46bb13f1e6bd119174ccf6d0
Diego Russo <diego25.russo@gmail.com>
2022-02-22 13:56:13 +0000

    routes home, contact page icons added
    Co-authored-by: Luna Yu <93207121+lunay-y@users.noreply.github.com>

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,22 @@
 import './App.css';
 import Homepage from '../Homepage/Homepage.js';
+import ContactPage from '../ContactPage/ContactPage';
+import { Routes, Route, Link } from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
-      <Homepage />
+     
+  
+      <Link to="/contact">ContactPage</Link>
+    
+    
+      <Routes>
+        <Route path="/" element={<Homepage/>}/>
+        <Route path="/contact" element={<ContactPage/>}/>
+        
+      </Routes>
     </div>
   );
 }
