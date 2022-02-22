21cdf64c101d1e717f053d97747d03f329978fe0
sandraskolarczyk <93612381+sandiskolarczyk@users.noreply.github.com>
2022-02-22 12:22:36 +0000

    Add images to the data

---
+++ src/libs/items.js
@@ -0,0 +1,112 @@
+const items = [
+  {
+    itemId: 1,
+    title: 'Pink Bomber Jacker',
+    location: 'London',
+    image: {
+      img: './images/bomber jacket.jpeg',
+      alt: 'Pink bomber jacket',
+    },
+  },
+  {
+    itemId: 2,
+    title: 'Wooden Night Stand',
+    location: 'Birmingham',
+    image: {
+      img: './images/wooden night stand.jpeg',
+      alt: 'Wooden night stand',
+    },
+  },
+  {
+    itemId: 3,
+    title: 'Old PS4 Console',
+    location: 'Liverpool',
+    image: {
+      img: './images/old console.jpeg',
+      alt: 'PS4 console',
+    },
+  },
+  {
+    itemId: 4,
+    title: 'Vintage Mirror with Gold Frame',
+    location: 'Brighton',
+    image: {
+      img: './images/vintage mirror.jpeg',
+      alt: 'Mirror with gold frame',
+    },
+  },
+  {
+    itemId: 5,
+    title: 'Unicorn Backpack for Kids',
+    location: 'Newcastle',
+    image: {
+      img: "./images/kid's backpack.jpeg",
+      alt: 'Unicorn backpack',
+    },
+  },
+  {
+    itemId: 6,
+    title: 'Pair of Coffee/Tea Mugs',
+    location: 'Nottingham',
+    image: {
+      img: './images/mugs.jpeg',
+      alt: 'White mugs for tea and coffee',
+    },
+  },
+  {
+    itemId: 7,
+    title: '',
+    location: '',
+    image: {
+      img: '',
+      alt: '',
+    },
+  },
+  {
+    itemId: 8,
+    title: '',
+    location: '',
+    image: {
+      img: '',
+      alt: '',
+    },
+  },
+  {
+    itemId: 9,
+    title: '',
+    location: '',
+    image: {
+      img: '',
+      alt: '',
+    },
+  },
+  {
+    itemId: 10,
+    title: '',
+    location: '',
+    image: {
+      img: '',
+      alt: '',
+    },
+  },
+  {
+    itemId: 11,
+    title: '',
+    location: '',
+    image: {
+      img: '',
+      alt: '',
+    },
+  },
+  {
+    itemId: 12,
+    title: '',
+    location: '',
+    image: {
+      img: '',
+      alt: '',
+    },
+  },
+];
+
+export default items;
