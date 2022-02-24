7f3ac315dc3825e48e0f8de2bcc898738f2f1034
mmuller2 <68875633+mmuller2@users.noreply.github.com>
2022-02-24 12:19:49 +0000

    funciotnality of contactPage
    Co-authored-by: Alex Mulgrew <93202458+apmcoding@users.noreply.github.com>

--- src/libs/items.js
+++ src/libs/items.js
@@ -3,6 +3,8 @@ const items = [
     itemId: 1,
     title: 'Pink Bomber Jacker',
     location: 'London',
+    email: 'billybob@gmails.com',
+    phone: '0789369923',
     image: {
       img: './images/bomber jacket.jpeg',
       alt: 'Pink bomber jacket',
@@ -12,6 +14,8 @@ const items = [
     itemId: 2,
     title: 'Wooden Night Stand',
     location: 'Birmingham',
+    email: 'billybob@gmails.com',
+    phone: '0789369912',
     image: {
       img: './images/wooden night stand.jpeg',
       alt: 'Wooden night stand',
@@ -21,6 +25,8 @@ const items = [
     itemId: 3,
     title: 'Old PS4 Console',
     location: 'Liverpool',
+    email: 'billybob@gmails.com',
+    phone: '0789369000',
     image: {
       img: './images/old console.jpeg',
       alt: 'PS4 console',
@@ -30,6 +36,8 @@ const items = [
     itemId: 4,
     title: 'Vintage Mirror with Gold Frame',
     location: 'Brighton',
+    email: 'billybob@gmails.com',
+    phone: '0789369999',
     image: {
       img: './images/vintage mirror.jpeg',
       alt: 'Mirror with gold frame',
@@ -39,6 +47,8 @@ const items = [
     itemId: 5,
     title: 'Unicorn Backpack for Kids',
     location: 'Newcastle',
+    email: 'billybob@gmails.com',
+    phone: '0789369988',
     image: {
       img: "./images/kid's backpack.jpeg",
       alt: 'Unicorn backpack',
@@ -48,6 +58,8 @@ const items = [
     itemId: 6,
     title: 'Pair of Coffee/Tea Mugs',
     location: 'Nottingham',
+    email: 'billybob@gmails.com',
+    phone: '0789369977',
     image: {
       img: './images/mugs.jpeg',
       alt: 'White mugs for tea and coffee',
@@ -57,6 +69,8 @@ const items = [
     itemId: 7,
     title: 'Black Mountain Bike',
     location: 'Oxford',
+    email: 'billybob@gmails.com',
+    phone: '0789369977',
     image: {
       img: './images/bike.jpg',
       alt: 'black mountain bike',
@@ -66,6 +80,8 @@ const items = [
     itemId: 8,
     title: 'Black Sony Camera',
     location: 'Cambridge',
+    email: 'billybob@gmails.com',
+    phone: '0789369966',
     image: {
       img: './images/camera.jpg',
       alt: 'Professional Camera',
@@ -75,6 +91,8 @@ const items = [
     itemId: 9,
     title: 'Grey Chair',
     location: 'Surrey',
+    email: 'billybob@gmails.com',
+    phone: '0789369955',
     image: {
       img: './images/chair.jpg',
       alt: 'Small grey chair',
@@ -84,6 +102,8 @@ const items = [
     itemId: 10,
     title: 'Dice for Board Games',
     location: 'Sheffield',
+    email: 'billybob@gmails.com',
+    phone: '0789369944',
     image: {
       img: './images/dice.jpg',
       alt: 'collection of dices',
@@ -93,6 +113,8 @@ const items = [
     itemId: 11,
     title: 'Gaming Mouse',
     location: 'Leicester',
+    email: 'billybob@gmails.com',
+    phone: '0789369933',
     image: {
       img: './images/mouse.jpg',
       alt: 'mouse for computer gamers',
@@ -102,6 +124,8 @@ const items = [
     itemId: 12,
     title: 'Nike Trainers',
     location: 'Kent',
+    email: 'billybob@gmails.com',
+    phone: '0789369922',
     image: {
       img: './images/trainers.jpg',
       alt: 'Nike trainers',
