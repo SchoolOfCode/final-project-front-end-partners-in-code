324a3e21c3e2395df6cfc7e372d1fad30d3e09c1
Luna Yu <yuanluna.yu@hotmail.it>
2022-02-24 16:24:20 +0000

    added responsive background to contact page
     Co-authored-by: Max Muller <fasttegr@gmail.com>

--- src/components/ContactPage/ContactPage.js
+++ src/components/ContactPage/ContactPage.js
@@ -3,11 +3,12 @@ import { Link } from 'react-router-dom';
 import { VscArrowLeft } from 'react-icons/vsc';
 import Button from '../Global/Button/Button';
 import items from '../../libs/items';
+import css from './ContactPage.module.css';
 
 export default function ContactPage({ phone, email }) {
   return (
-    <div>
-      <Link to="/">
+    <div className={css.contactContainer}>
+      <Link to="/items">
         <Button text={<VscArrowLeft />} />
       </Link>
 
