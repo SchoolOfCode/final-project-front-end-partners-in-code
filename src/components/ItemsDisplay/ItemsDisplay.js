8f8850da2052fe398c60cd99964bb59c7de2387e
sandraskolarczyk <93612381+sandiskolarczyk@users.noreply.github.com>
2022-02-24 15:31:50 +0000

    Apply css module to ItemDisplay component

--- src/components/ItemsDisplay/ItemsDisplay.js
+++ src/components/ItemsDisplay/ItemsDisplay.js
@@ -1,32 +1,27 @@
 import Logo from '../Global/Logo/Logo.js';
 import H1 from '../Global/H1/H1.js';
-import AddNewItem from "../AddNewItem/AddNewItem.js"
-import ListItem from '../ListItem/ListItem.js';
 import AddNewItem from '../AddNewItem/AddNewItem.js';
+import ListItem from '../ListItem/ListItem.js';
+
+import css from './ItemsDisplay.module.css';
 
 export default function ItemsDisplay({ items, onAddNewItem }) {
   return (
-
-    <div className="display-container">
-      <AddNewItem onAddNewItem={onAddNewItem}/>
+    <div className={css.displayContainer}>
+      <AddNewItem onAddNewItem={onAddNewItem} />
       <div className="box">
         <Logo />
       </div>
-      <div className="browse">
-
+      <div className={css.browse}>
         <H1 text="Browse..." />
       </div>
-      <div className='title'>
+      <div className={css.title}>
         <ListItem items={items} />
       </div>
 
-      <div className='plus-button'>
-        <AddNewItem className='plus-button'/>
+      <div className={css.plusButton}>
+        <AddNewItem className="plus-button" />
       </div>
-     
-
     </div>
-    
-   
   );
 }
--- src/components/ListItem/ListItem.js
+++ src/components/ListItem/ListItem.js
@@ -1,24 +1,11 @@
 import Item from '../Item/Item.js';
 
-
-
 export default function ListItem({ items }) {
   return (
     <div className="container">
       {items.map(function (item) {
         return (
-
-        
-            <div className='responsive-container'> <Item
-            key={item.itemId}
-            img={item.image.img}
-            alt={item.image.alt}
-            title={item.title}
-            location={item.location}
-          /></div>
-         
-         
-
+          <div className="responsive-container">
             <Item
               key={item.itemId}
               img={item.image.img}
@@ -26,8 +13,7 @@ export default function ListItem({ items }) {
               title={item.title}
               location={item.location}
             />
-    
-
+          </div>
         );
       })}
     </div>
