caa6f3f1802f7e071cf61d24a2e9c10cdf78967f
sandraskolarczyk <93612381+sandiskolarczyk@users.noreply.github.com>
2022-02-25 10:16:21 +0000

    Style give and browse buttons; main heading and secondary heading Co-authored-by: Niro Sarva <94461105+NiroSarva@users.noreply.github.com>

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -5,7 +5,7 @@ import H2 from '../Global/H2/H2.js';
 import Button from '../Global/Button/Button.js';
 import { Link } from 'react-router-dom';
 
-import css from './Homepage.module.css'
+import css from './Homepage.module.css';
 
 function Homepage() {
   return (
@@ -13,25 +13,37 @@ function Homepage() {
       <div className={css.logo}>
         <Logo />
       </div>
-      <Button text="Log In" />
-      <Button text="Sign Up" />
+      <div className={css.twoAuthButtons}>
+        <div className={css.pinkButtons}>
+          <div className={css.logInButton}>
+            <Button text="Log In" />
+          </div>
+          <div className={css.signUpButton}>
+            <Button text="Sign Up" />
+          </div>
+        </div>
+      </div>
       <div className={css.h1}>
-      <H1 text="Exchange and Share" />
+        <H1 text="Exchange and Share" />
       </div>
       <div className={css.h2}>
-      <H2 text="Give new life to objects, share what you have, love what others have loved." />
+        <H2 text="Give new life to objects, share what you have, love what others have loved." />
       </div>
-      <div className={css.greenButtons}>
-      <Link to="items">
-      <div className={css.giveButton}>
-        <Button text="+ Give" />
+      <div className={css.twoButtons}>
+        <div className={css.greenButtons}>
+          <Link to="items">
+            <div className={css.giveButton}>
+              <Button text="+ Give" />
+            </div>
+          </Link>
+
+          <p>or</p>
+          <Link to="items">
+            <div className={css.browseButton}>
+              <Button text="Browse >>" />
+            </div>
+          </Link>
         </div>
-      </Link>
-      <Link to="items">
-      <div className={css.browseButton}>
-        <Button text="Browse >>" />
-        </div>
-      </Link>
       </div>
     </div>
   );
