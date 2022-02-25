65c85656a631a1fadf48374b50eafef1a2727635
sandraskolarczyk <93612381+sandiskolarczyk@users.noreply.github.com>
2022-02-25 12:15:33 +0000

    Add comments; Add hover style to buttons

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -9,13 +9,13 @@ import css from './Homepage.module.css';
 
 function Homepage() {
   return (
-    <div classname={css.homepageContainer}>
+    <div className={css.homepageContainer}>
       <div className={css.logoContainer}>
-      <div className={css.logoItem}>
-      <div className={css.logo}>
-        <Logo />
-      </div>
-      </div>
+        <div className={css.logoItem}>
+          <div className={css.logo}>
+            <Logo />
+          </div>
+        </div>
         <div className={css.pinkButtons}>
           <div className={css.logInButton}>
             <Button text="Log In" />
