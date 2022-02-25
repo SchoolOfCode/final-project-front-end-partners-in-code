95ed75ae85274545e52a8e3ca383365c1963894d
Alex Mulgrew <93202458+apmcoding@users.noreply.github.com>
2022-02-25 12:31:04 +0000

    responsive background sorted

--- src/components/ListItem/ListItem.js
+++ src/components/ListItem/ListItem.js
@@ -2,20 +2,22 @@ import Item from '../Item/Item.js';
 import css from './ListItem.module.css';
 export default function ListItem({ items }) {
   return (
-    <div className={css.container}>
-      {items.map(function (item) {
-        return (
-          <div className={css.responsiveContainer}>
-            <Item
-              key={item.itemId}
-              img={item.image.img}
-              alt={item.image.alt}
-              title={item.title}
-              location={item.location}
-            />
-          </div>
-        );
-      })}
+    <div className={css.parentContainer}>
+      <div className={css.container}>
+        {items.map(function (item) {
+          return (
+            <div className={css.responsiveContainer}>
+              <Item
+                key={item.itemId}
+                img={item.image.img}
+                alt={item.image.alt}
+                title={item.title}
+                location={item.location}
+              />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
