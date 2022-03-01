22592100d8a570e666c44e729cdfe2dfe35975a5
Luna Yu <yuanluna.yu@hotmail.it>
2022-03-01 12:11:48 +0000

    change background in Browse page
    Co-authored-by: Diego Russo <diego.russo@gmail.com>

--- src/components/ItemsDisplay/ItemsDisplay.js
+++ src/components/ItemsDisplay/ItemsDisplay.js
@@ -3,25 +3,34 @@ import H1 from '../Global/H1/H1.js';
 import AddNewItem from '../AddNewItem/AddNewItem.js';
 import ListItem from '../ListItem/ListItem.js';
 import css from './ItemsDisplay.module.css';
+import Button from '../Global/Button/Button.js';
 
 export default function ItemsDisplay({ items, onAddNewItem }) {
   return (
     <div className={css.displayContainer}>
-      
       <div className={css.box}>
         <Logo />
+        <div className={css.pinkButtons}>
+          <div className={css.logInButton}>
+            <Button text="Log In" />
+          </div>
+          <div className={css.signUpButton}>
+            <Button text="Sign Up" />
+          </div>
+        </div>
       </div>
       <div className={css.browse}>
         <H1 text="Browse..." />
       </div>
+
       <div className={css.section}>
-      <div className={css.itemDisplayContainer}>
+        <div className={css.itemDisplayContainer}>
           <ListItem items={items} />
+        </div>
       </div>
-      </div>
-     
+
       <div className={css.buttonContainer}>
-          <AddNewItem onAddNewItem={onAddNewItem} />
+        <AddNewItem onAddNewItem={onAddNewItem} />
       </div>
     </div>
   );
