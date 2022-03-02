0ad3435e6d053a8069b24f0334a8180451c0da66
mmuller2 <68875633+mmuller2@users.noreply.github.com>
2022-03-02 11:06:56 +0000

    remove profile displayed on contact page

--- src/components/Auth0/UserProfile/Profile.js
+++ src/components/Auth0/UserProfile/Profile.js
@@ -3,7 +3,7 @@ import { useAuth0 } from '@auth0/auth0-react';
 
 const Profile = () => {
   const { user, isAuthenticated, isLoading } = useAuth0();
-  console.log(user)
+  console.log(user);
 
   if (isLoading) {
     return <div>Loading ...</div>;
@@ -14,7 +14,7 @@ const Profile = () => {
       <div>
         <img src={user.picture} alt={user.name} />
         <h2>{user.name}</h2>
-        <h3>{user.user_id}</h3>
+        <h3>{user.sub}</h3>
         <p>{user.email}</p>
       </div>
     )
--- src/components/ContactPage/ContactPage.js
+++ src/components/ContactPage/ContactPage.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Logo from '../Global/Logo/Logo.js';
 import Button from '../Global/Button/Button';
 import items from '../../libs/items';
 import css from './ContactPage.module.css';
-import Profile from '../Auth0/UserProfile/Profile.js'
 
 export default function ContactPage({ phone, email }) {
   return (
@@ -17,7 +15,6 @@ export default function ContactPage({ phone, email }) {
         <p>{items[0].phone}</p>
         <p>{items[0].email}</p>
       </div>
-      <Profile/>
     </div>
   );
 }
