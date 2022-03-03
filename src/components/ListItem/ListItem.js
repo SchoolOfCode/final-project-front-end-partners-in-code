0e51e2af958bacd04eb5960293ad5681a6eb81f8
mmuller2 <68875633+mmuller2@users.noreply.github.com>
2022-03-03 13:41:01 +0000

    items page click disabled if not logged in
    Co-authored-by: Diego Russo <diego.russo@gmail.com>
    Co-authored-by: Alex Mulgrew <93202458+apmcoding@users.noreply.github.com>

--- src/components/ListItem/ListItem.js
+++ src/components/ListItem/ListItem.js
@@ -1,13 +1,30 @@
 import Item from '../Item/Item.js';
 import { Link } from 'react-router-dom';
+import { useAuth0 } from '@auth0/auth0-react';
 
+// const { isAuthenticated } = useAuth0();
 export default function ListItem({ items }) {
+  const { isAuthenticated } = useAuth0();
   return (
     <>
       {items.map(function (item) {
-        return (
-          <div>
-            <Link to="/contact" state={{ from: item }}>
+        if (isAuthenticated) {
+          return (
+            <div>
+              <Link to="/contact" state={{ from: item }}>
+                <Item
+                  key={item.itemId}
+                  img={item.image.img}
+                  alt={item.alt}
+                  title={item.title}
+                  location={item.location}
+                />
+              </Link>
+            </div>
+          );
+        } else {
+          return (
+            <div>
               <Item
                 key={item.itemId}
                 img={item.image.img}
@@ -15,9 +32,9 @@ export default function ListItem({ items }) {
                 title={item.title}
                 location={item.location}
               />
-            </Link>
-          </div>
-        );
+            </div>
+          );
+        }
       })}
     </>
   );
