cf5879e545eff5fed44bbe65eccb62c9353611a2
Luna Yu <yuanluna.yu@hotmail.it>
2022-03-05 12:10:34 +0000

    created data for aboutUs
    Co-authored-by: Sandra Skolarczyk <93612381+sandiskolarczyk@users.noreply.github.com>

---
+++ src/libs/aboutUs.js
@@ -0,0 +1,59 @@
+const aboutUs = [
+  {
+    image: {
+      img: './images/chair.jpg',
+      alt: 'Small grey chair',
+    },
+    name: 'Niro Sarva',
+    github: 'https://github.com/lunay-y',
+    linkedin: 'https://www.linkedin.com/in/yuanlunayu/',
+  },
+  {
+    image: {
+      img: './images/chair.jpg',
+      alt: 'Small grey chair',
+    },
+    name: 'Sandra Skolarczyk',
+    github:
+      'https://github.com/sandiskolarczyk?tab=overview&from=2022-03-01&to=2022-03-04',
+    linkedin: 'https://www.linkedin.com/in/sandraskolarczyk/',
+  },
+  {
+    image: {
+      img: './images/chair.jpg',
+      alt: 'Small grey chair',
+    },
+    name: 'Alex Mulgrew',
+    github: 'https://github.com/',
+    linkedin: 'https://www.linkedin.com/',
+  },
+  {
+    image: {
+      img: './images/chair.jpg',
+      alt: 'Small grey chair',
+    },
+    name: 'Yuan Luna Yu',
+    github: 'https://github.com/lunay-y',
+    linkedin: 'https://www.linkedin.com/in/yuanlunayu/',
+  },
+  {
+    image: {
+      img: './images/chair.jpg',
+      alt: 'Small grey chair',
+    },
+    name: 'Diego Russo',
+    github: 'https://github.com/',
+    linkedin: 'https://www.linkedin.com/',
+  },
+  {
+    image: {
+      img: './images/chair.jpg',
+      alt: 'Small grey chair',
+    },
+    name: 'Max Muller',
+    github: 'https://github.com/lunay-y',
+    linkedin: 'https://www.linkedin.com/in/yuanlunayu/',
+  },
+];
+
+export default aboutUs;
