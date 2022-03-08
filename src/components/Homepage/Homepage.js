1aff2240c88bd53d139afcca1142156fff22f794
mmuller2 <68875633+mmuller2@users.noreply.github.com>
2022-03-08 16:31:11 +0000

    signoutbutton removed once logged in working
    Co-authored-by: Niro Sarva <94461105+NiroSarva@users.noreply.github.com>
    Co-authored-by: Alex Mulgrew <93202458+apmcoding@users.noreply.github.com>

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -4,62 +4,113 @@ import H1 from '../Global/H1/H1.js';
 import H2 from '../Global/H2/H2.js';
 import Button from '../Global/Button/Button.js';
 import { Link } from 'react-router-dom';
-import Auth0LoginButton from '../Auth0/Auth0LoginButton/Auth0Login.js';
-import Auth0LogoutButton from '../Auth0/Auth0LogoutButton/Auth0Logout.js';
 import Auth0GiveButton from '../Auth0/Auth0GiveButton/Auth0Give.js';
 import AuthenticationButton from '../Auth0/Auth0InOutButton/Auth0InOut.js';
 import css from './Homepage.module.css';
-import SignupButton from '../Auth0/Auth0SignUpButton/Auth0SignUp.js'
+import SignupButton from '../Auth0/Auth0SignUpButton/Auth0SignUp.js';
+import { useAuth0 } from '@auth0/auth0-react';
 
 function Homepage() {
-  return (
-        <div className={css.homepageContainer}>
-    <div className={css.logoContainer}>
-        <div className={css.logoItem}>
-          <div className={css.logo}>
-            <Logo />
+  const { isAuthenticated } = useAuth0();
+  if (isAuthenticated) {
+    return (
+      <div className={css.homepageContainer}>
+        <div className={css.logoContainer}>
+          <div className={css.logoItem}>
+            <div className={css.logo}>
+              <Logo />
+            </div>
           </div>
-        </div>
-        <div className={css.pinkButtons}>
-          <div className={css.logInButton}>
-            {/* check to make sure button works, original button commented */}
-            {/* <Button text="Log In" /> */}
-            {/* <Auth0LoginButton />
+          <div className={css.pinkButtons}>
+            <div className={css.logInButton}>
+              {/* check to make sure button works, original button commented */}
+              {/* <Button text="Log In" /> */}
+              {/* <Auth0LoginButton />
             <Auth0LogoutButton /> */}
-            {/* the login and log out buttons replaced by the line below */}
-            <AuthenticationButton />
-          </div>
-          <div className={css.signUpButton}>
-            {/* <Button text="Sign Up" /> */}
-            <SignupButton/>
+              {/* the login and log out buttons replaced by the line below */}
+              <AuthenticationButton />
+            </div>
+            {/* <div className={css.signUpButton}>
+              <Button text="Sign Up" />
+              <SignupButton />
+            </div> */}
           </div>
         </div>
-      </div>
-      <div className={css.h1}>
-        <H1 text="Exchange and Share" />
-      </div>
-      <div className={css.h2}>
-        <H2 text="Give new life to objects, share what you have, love what others have loved." />
-      </div>
-      <div className={css.twoButtons}>
-        <div className={css.greenButtons}>
-          <Link to="items">
-            <div className={css.giveButton}>
-              {/* <Button text="+ Give" /> */}
-              <Auth0GiveButton />
-            </div>
-          </Link>
+        <div className={css.h1}>
+          <H1 text="Exchange and Share" />
+        </div>
+        <div className={css.h2}>
+          <H2 text="Give new life to objects, share what you have, love what others have loved." />
+        </div>
+        <div className={css.twoButtons}>
+          <div className={css.greenButtons}>
+            <Link to="items">
+              <div className={css.giveButton}>
+                {/* <Button text="+ Give" /> */}
+                <Auth0GiveButton />
+              </div>
+            </Link>
 
-          <p>or</p>
-          <Link to="items">
-            <div className={css.browseButton}>
-              <Button text="Browse >>" />
-            </div>
-          </Link>
+            <p>or</p>
+            <Link to="items">
+              <div className={css.browseButton}>
+                <Button text="Browse >>" />
+              </div>
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className={css.homepageContainer}>
+        <div className={css.logoContainer}>
+          <div className={css.logoItem}>
+            <div className={css.logo}>
+              <Logo />
+            </div>
+          </div>
+          <div className={css.pinkButtons}>
+            <div className={css.logInButton}>
+              {/* check to make sure button works, original button commented */}
+              {/* <Button text="Log In" /> */}
+              {/* <Auth0LoginButton />
+            <Auth0LogoutButton /> */}
+              {/* the login and log out buttons replaced by the line below */}
+              <AuthenticationButton />
+            </div>
+            <div className={css.signUpButton}>
+              {/* <Button text="Sign Up" /> */}
+              <SignupButton />
+            </div>
+          </div>
+        </div>
+        <div className={css.h1}>
+          <H1 text="Exchange and Share" />
+        </div>
+        <div className={css.h2}>
+          <H2 text="Give new life to objects, share what you have, love what others have loved." />
+        </div>
+        <div className={css.twoButtons}>
+          <div className={css.greenButtons}>
+            <Link to="items">
+              <div className={css.giveButton}>
+                {/* <Button text="+ Give" /> */}
+                <Auth0GiveButton />
+              </div>
+            </Link>
+
+            <p>or</p>
+            <Link to="items">
+              <div className={css.browseButton}>
+                <Button text="Browse >>" />
+              </div>
+            </Link>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Homepage;
--- src/components/ItemsDisplay/ItemsDisplay.js
+++ src/components/ItemsDisplay/ItemsDisplay.js
@@ -7,41 +7,81 @@ import AuthenticationButton from '../Auth0/Auth0InOutButton/Auth0InOut.js';
 import SignupButton from '../Auth0/Auth0SignUpButton/Auth0SignUp.js';
 import { Link } from 'react-router-dom';
 import NavBar from '../NavBar/NavBar.js';
+import { useAuth0 } from '@auth0/auth0-react';
 
 export default function ItemsDisplay({ items, onAddNewItem }) {
-  return (
-    <div className={css.displayContainer} data-testid="list-item">
-      <div className={css.box}>
-        <div className={css.logoItem}>
-          <div className={css.logo}>
-            <Link to="/">
-              <Logo />
-            </Link>
+  const { isAuthenticated } = useAuth0();
+  if (isAuthenticated) {
+    return (
+      <div className={css.displayContainer} data-testid="list-item">
+        <div className={css.box}>
+          <div className={css.logoItem}>
+            <div className={css.logo}>
+              <Link to="/">
+                <Logo />
+              </Link>
+            </div>
           </div>
-        </div>
-        <div className={css.pinkButtons}>
-          <div className={css.logInButton}>
-            <AuthenticationButton />
-          </div>
-          <div className={css.signUpButton}>
+          <div className={css.pinkButtons}>
+            <div className={css.logInButton}>
+              <AuthenticationButton />
+            </div>
+            {/* <div className={css.signUpButton}>
             <SignupButton />
-          </div>
-          <div className={css.navBar}>
-            <NavBar />
+          </div> */}
+            <div className={css.navBar}>
+              <NavBar />
+            </div>
           </div>
         </div>
-      </div>
-      <div className={css.browse}>
-        <H1 text="Browse..." />
-      </div>
-      <div className={css.section}>
-        <div className={css.itemDisplayContainer}>
-          <ListItem items={items} />
+        <div className={css.browse}>
+          <H1 text="Browse..." />
+        </div>
+        <div className={css.section}>
+          <div className={css.itemDisplayContainer}>
+            <ListItem items={items} />
+          </div>
+        </div>
+        <div className={css.buttonContainer}>
+          <AddNewItem onAddNewItem={onAddNewItem} />
         </div>
       </div>
-      <div className={css.buttonContainer}>
-        <AddNewItem onAddNewItem={onAddNewItem} />
+    );
+  } else {
+    return (
+      <div className={css.displayContainer} data-testid="list-item">
+        <div className={css.box}>
+          <div className={css.logoItem}>
+            <div className={css.logo}>
+              <Link to="/">
+                <Logo />
+              </Link>
+            </div>
+          </div>
+          <div className={css.pinkButtons}>
+            <div className={css.logInButton}>
+              <AuthenticationButton />
+            </div>
+            <div className={css.signUpButton}>
+              <SignupButton />
+            </div>
+            <div className={css.navBar}>
+              <NavBar />
+            </div>
+          </div>
+        </div>
+        <div className={css.browse}>
+          <H1 text="Browse..." />
+        </div>
+        <div className={css.section}>
+          <div className={css.itemDisplayContainer}>
+            <ListItem items={items} />
+          </div>
+        </div>
+        <div className={css.buttonContainer}>
+          <AddNewItem onAddNewItem={onAddNewItem} />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
